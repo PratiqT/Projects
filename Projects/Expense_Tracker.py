b7a93e7651c92c9073b10bccdea2ca0230961141
DhuandharCoder <pt4597591@gmail.com>
2026-09-17 12:06:32 +0530

    .

--- Projects/Expense_Tracker.py
+++ Projects/Expense_Tracker.py
@@ -34,12 +34,13 @@ def view_expenses():
     global expenses
     
     for i, expense in enumerate(expenses, 1):
-            print(f"Expense #{i}")
-            print("-------------------------")
+            print(f"╔══════════════════════════╗")
+            print(f"║        EXPENSE #{i}      ║")
+            print("f╠══════════════════════════╣")
             print(f"Category: {expense['category']}")
             print(f"Amount: ₹{expense['amount']}")
             print(f"Description: {expense['description']}")
-            print("-------------------------\n")
+            print(f"╚══════════════════════════╝")
 
      
 
