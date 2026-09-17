48e0e9f08f203badfeaf43da676364d95ea8bd29
DhuandharCoder <pt4597591@gmail.com>
2026-09-17 12:27:47 +0530

    .

--- Projects/Expense_Tracker.py
+++ Projects/Expense_Tracker.py
@@ -29,20 +29,18 @@ def add_expense():
     expenses.append(entry)
     print("expense added successfully!\n")
 
-
+#desiner way to show the Expense
 def view_expenses():
     global expenses
     
     for i, expense in enumerate(expenses, 1):
-           for i, expense in enumerate(expenses, 1):
-
-            print("╔═════════════════════════════════════╗")
-            print(f"║        EXPENSE #{i:<20}║")
-            print("╠═════════════════════════════════════╣")
-            print(f"║ Category: {expense['category']:<20}║")
-            print(f"║ Amount: ₹{expense['amount']:<20}║")
-            print(f"║ Description: {expense['description']:<16}║")
-            print("╚═════════════════════════════════════╝")
+        print("╔═════════════════════════════════════╗")
+        print(f"║        EXPENSE #{i:<20}║")
+        print("╠═════════════════════════════════════╣")
+        print(f"║ Category: {expense['category']:<20}║")
+        print(f"║ Amount: ₹{expense['amount']:<20}║")
+        print(f"║ Description: {expense['description']:<16}║")
+        print("╚═════════════════════════════════════╝")
 
      
 
